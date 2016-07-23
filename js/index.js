d9c9583b93627df86604c0befa6c1a864076f2dd
Ask Prateek <Ask Prateek>
2016-07-23 20:40:55 +0530

    Phase 1 Complete

---
+++ js/index.js
@@ -0,0 +1,217 @@
+/* ---- Terminal Window Configurtaion ---- */
+var directory = './content/';
+$(document).ready(function(){
+        $.ajax({
+          url: directory,
+          success: function(data){
+            var folder;
+            $(data).find("td > a").each(function(){
+              folder=$(this).attr("href");
+              console.log(folder);
+              folder=$(this).attr("href");
+
+            });
+            }
+          });
+    });
+var files = document.getElementById('files').innerHTML.split(" ");
+//console.log(files);
+
+/*   Tab Key Funtionality    */
+command.addEventListener("keydown", function (e) {
+  /*  ALT - 18, TAB - 9 , CTRL - 17  */
+  if (e.keyCode === 9) {
+    e.preventDefault();
+
+    var value = document.getElementById('command').value;
+    var user_input= value.split(" ");
+    console.log(user_input);
+    var i=0; var file=user_input[0] + " ";
+    if (user_input[1].length){
+      for (i=0; i <files.length; i++){
+        if ( files[i].startsWith(user_input[1])  ){
+          file += files[i];
+        }
+      }
+
+    }
+    if (file.length){
+      document.getElementById('command').value = file;
+    }
+  }
+});
+var cat = {
+  'about' : "20, B.Tech - CE, Web|Graphics Designer|Developer",
+  'contact': "Email - dap@askprateek.com ",
+  'projects': 'Home Automation System - Hackon - Treasure Hunt - Graph Plotter - URL Shortner',
+  'skills' : 'Python - Django - HTML - CSS - JS - Ruby on Rails - Adobe Photoshop',
+}
+//console.log(cat.about);
+
+/*  ENTER Key Press | Funtionality of Commands   */
+command.addEventListener("keydown", function (e) {
+  /*  ALT - 18, TAB - 9 , CTRL - 17 , ENTER - 13 */
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    var value = document.getElementById('command').value;
+    var user_input= value.split(" ");
+  //  console.log(user_input);
+
+  }
+});
+
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("./content/about.txt");
+
+
+/* ---- particles.js config ---- */
+
+particlesJS("aa_particles", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+
+/* ---- stats.js config ---- */
+
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
