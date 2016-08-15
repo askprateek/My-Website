e5dfa192065c23d5836c7c3ab41747f98f872b6d
Ask Prateek <Ask Prateek>
2016-08-15 19:39:08 +0530

    Prduction | Local server Fix

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-/* ---- Terminal Window Configurtaion ---- */
-var directory = 'content/';
-var path='';
-var login = '<span class="user"><b>dap</b>@askprateek.com:~ ';
-var bash = document.getElementById('root').innerHTML;
-var lastcommands=['ls',];
-var commandindex=1;
+
 //$(document).ready(function(){
 function getdir(dir){
   var dir_list = [];
@@ -13,7 +7,7 @@ function getdir(dir){
     async: false,
     success: function(data){
       var folder;
-      $(data).find("td > a").each(function(){
+      $(data).find(element_to_search).each(function(){
         folder=$(this).attr('href');
         //console.log(folder);
         dir_list.push(folder);
@@ -67,7 +61,7 @@ function setcommand(index){
 }
 
 function openHelp(){
-  readHelpFile('../askprateek/help.txt');
+  readHelpFile('terminal/help.txt');
   $('#help').openModal();
 }
 /* Read File  */
@@ -182,7 +176,7 @@ command.addEventListener("keydown", function (e) {
           $('#term').append('<p>cd: '+user_input[1]+': Not a directory</p>');
         }
         else if ( (user_input[1]=='..') || (user_input[1]=='/') ){
-          directory = 'content/';
+          directory = 'terminal/content/';
           path ='';
           appendCommand(command);
           changeCommand(path);
@@ -200,7 +194,7 @@ command.addEventListener("keydown", function (e) {
       case 'help':
         appendCommand(command);
         autoscroll();
-        readHelpFile('../askprateek/help.txt');
+        readHelpFile('terminal/help.txt');
         $('#help').openModal();
 
       //  readTextFile('../askprateek/help.txt', command);
@@ -248,146 +242,3 @@ command.addEventListener("keydown", function (e) {
 
   }
 });
-
-
-
-
-/* ---- particles.js config ---- */
-
-particlesJS("aa_particles", {
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-
-
-/* ---- stats.js config ----
-
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update); */
-var cat = {
-  'about' : "20, B.Tech - CE, Web|Graphics Designer|Developer",
-  'contact': "Email - dap@askprateek.com ",
-  'projects': 'Home Automation System - Hackon - Treasure Hunt - Graph Plotter - URL Shortner',
-  'skills' : 'Python - Django - HTML - CSS - JS - Ruby on Rails - Adobe Photoshop',
-}
