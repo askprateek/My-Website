848cb0008b86f4e21699ba8f9886dbe0346d3333
askprateek <askpk123@gmail.com>
2016-09-26 20:39:24 +0530

    Contact Info Updated

--- terminal/sections/z-contact.php
+++ terminal/sections/z-contact.php
@@ -2,8 +2,29 @@
     
     <div class="row">
         <div class="col s12 center-align">
-            <h2>Hire me or Just say Hi. God knows, We might end up building memories :b</h2>
+            <h2>Hire me or Just say Hi. Who knows, We might end up building memories :b</h2>
             <h1>dap@askprateek.com</h1>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row center-align">
+            <div class="col s12 m2 offset-m1 links">
+                <a href="https://github.com/askprateek/">Github</a>
+            </div>
+            
+            <div class="col s12 m2 links">
+                <a href="https://www.hackerearth.com/@askpk123">Hackerearth</a>
+            </div>
+            <div class="col s12 m2 links">
+                <a href="https://www.linkedin.com/in/askprateek/">Linkedin</a>
+            </div>
+            <div class="col s12 m2 links">
+                <a href="https://facebook.com/askpk123">Facebook</a>
+            </div>
+            
+            <div class="col s12 m2 links">
+                <a href="https://twitter.com/AskPK123/">Twitter</a>
+            </div>
         </div>
     </div>
   
